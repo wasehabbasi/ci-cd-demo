934bfa40ed6a597e76deb6deb65df3b92294f0d9
wasehabbasi <54438639+wasehabbasi@users.noreply.github.com>
2025-12-25 18:36:06 +0500

    test pipeline

--- backend/server.js
+++ backend/server.js
@@ -9,5 +9,5 @@ app.use(express.json());
 app.use("/api/users", usersRoutes);
 
 app.listen(5000, () => {
-  console.log("Server running on port 5001");
+  console.log("Server running on port 5000");
 });
